3fdf655e9dc03591a22a8f0007c56328255daedd
NatalyGF <natagutty@gmail.com>
2026-03-03 16:23:32 -0500

    Clase de variables e imprimir en consola

--- js/variables.js
+++ js/variables.js
@@ -1,2 +1,30 @@
 console.log("Variables in JavaScript");
+//¿Que es una variable?
+//Un espacio reservado donde guardamos un valor
+// como string, number, booleano, array, objetos
+
+// tipos de variable: let, const, var
+let age = 35;
+console.log("age", age)
+
+const firstName ="Nataly";
+console.log("Mi nombre es: ", firstName)
+
+
+//Buenas practicas
+//  usar constantes en lo posible
+// nombrar las variables en camelCase
+// colocar nombres descriptivos
+
+//Ejercicio: Declarar una variable que describa a una persona e imprimir en consola
+
+const yourName = "Nataly";
+const lastName = "Gutierrez";
+let currentAge = 35;
+let estatura = 160;
+const isFemenine = true;
+
+console.log("La descripción de esta persona es: Su nombre y apellido es: ", yourName + " " + lastName +" tiene "+ currentAge + " años, su estatura es "+ estatura + " centimetros" + " y es mujer? "+ isFemenine)
+
+
 
