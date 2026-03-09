5e34e43dcd8b9614c805e8b5f1161353cd45039a
NatalyGF <natagutty@gmail.com>
2026-03-09 16:20:06 -0500

    monday class

--- js/arrowFunctions.js
+++ js/arrowFunctions.js
@@ -11,3 +11,34 @@ const verifyIsMajor = (age) => {
 }
 
 console.log(verifyIsMajor(19));
+
+//Operador ternario
+
+// expresion ? 'console.log("true")' : console.log("aqui");
+
+const verifyAge = (age, DNI) => {
+    age >= 18 && DNI ? console.log ("es mayor de edad") : console.log("es menor de edad")
+    // o puede ser
+    // return age >= 18 ? "es mayor de edad" : "es menor de edad"
+}
+
+verifyAge(18, true)
+
+////////////////////////////////////////
+
+const regalarleCarro = () => {
+    return "le regalo un carro"
+}
+
+const regalarleBici = () => {
+    return "le regalo una bici"
+}
+
+const verifyregalo = (age) => {
+    return age >= 18 ? regalarleCarro() : regalarleBici()
+    // o puede ser
+    // return age >= 18 ? "es mayor de edad" : "es menor de edad"
+}
+
+console.log(verifyregalo(18))
+
