5afa20cea378bdcdb480c53409c63cd5c86cb5bb
NatalyGF <natagutty@gmail.com>
2026-03-09 15:52:15 -0500

    new branch to arrow functions

--- js/arrowFunctions.js
+++ js/arrowFunctions.js
@@ -1,1 +1,13 @@
-console.log("Explicación función flecha");
+console.log("Bienvenidos a arrow functions")
+//Explicación arrow function
+
+//diferencia sintaxis
+
+const verifyIsMajor = (age) => {
+    if (age >= 18) {
+        return true;
+    }
+    return false
+}
+
+console.log(verifyIsMajor(19));
